458e132211883ec251bff37ec4511966b86db77d
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-12-08 22:45:24 -0500

    wrote passing tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,38 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if(item.enhancement < 20) {
+    const enhanced = item.enhancement + 1;
+    return { ...item,
+      enhancement: enhanced
+    }
+  } else {
+    return {...item}
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15) {
+    const weakened = item.durability - 5;
+    return {...item,
+    durability: weakened}
+  } else {
+    if(item.enhancement > 16) {
+      const weaker = item.durability - 10;
+      const lowered = item.enhancement - 1;
+      return {...item,
+      durability: weaker,
+    enhancement: lowered}
+    } else {
+      const weaker = item.durability - 10;
+      return { ...item,
+      durability: weaker}
+    }
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,44 @@
 const enhancer = require('./enhancer.js');
+const { TestScheduler } = require('jest');
 // test away!
+
+describe("enhancer tests", () => {
+    describe('tests repair', () => {
+        test('tests repair functionality', () => {
+            const item = enhancer.repair({itemName: "sword", durability: 50, enhancement: 15});
+            const durability = item.durability;
+            expect(durability).toBe(100);
+        })
+    })
+
+    describe('tests success', () => {
+        test('tests success functionality w/ enhancement lower than 20', () => {
+            const item = enhancer.success({itemName: "sword", durability: 50, enhancement: 15});
+            const enhancement = item.enhancement;
+            expect(enhancement).toBe(16);
+        });
+        test('tests success functionality w/ enhancement at 20', () => {
+            const item = enhancer.success({itemName: "sword", durability: 50, enhancement: 20});
+            const enhancement = item.enhancement;
+            expect(enhancement).toBe(20);
+        })
+    })
+
+    describe('tests fail', () => {
+        test('tests failure w/ durability less than 15', () => {
+            const item = enhancer.fail({itemName: "sword", durability: 50, enhancement: 14});
+            const durability = item.durability;
+            expect(durability).toBe(45);
+        });
+        test('tests failure w/ enhancement at 15', () => {
+            const item = enhancer.fail({itemName: "sword", durability: 50, enhancement: 15});
+            const durability = item.durability;
+            expect(durability).toBe(40);
+        });
+        test('tests failure w/ enhancement greater than 16', () => {
+            const item = enhancer.fail({itemName: "sword", durability: 50, enhancement: 18});
+            const enhancement = item.enhancement;
+            expect(enhancement).toBe(17);
+        }) 
+    })
+})
